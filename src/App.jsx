7f7914e8a0d81726bf07ed3fb78206949424fc84
albertoceriani <alcerianivr@gmail.com>
2026-06-11 18:04:39 +0200

    visualizzazione utenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 // import { useState } from "react";
 import "./App.css";
-import Biglietto from "./components/Biglietto";
-import Card from "./components/Card/Card";
-import Card2 from "./components/Card2/Card2";
-import MeteoInfo from "./components/MeteoInfo";
+import Utente from "./components/Utente/Utente";
 
 function App() {
   return (
     <>
-      <h1>PRIMO REACT</h1>
-      <Biglietto nome="Giacomo" destinazione="Roma"/>
-      <Biglietto nome="Emma" destinazione="Bari"/>
-      <Biglietto nome="Marta" destinazione="Como"/>
-      <Biglietto destinazione="Marrachesh"/>
-      <MeteoInfo/>
-      <Card/>
-      <Card2/>
+      <Utente/>      
     </>
   );
 }
