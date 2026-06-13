7bd7ef28cff6f44b4b5b48af37f4bd4512124edd
albertoceriani <alcerianivr@gmail.com>
2026-06-13 11:05:59 +0200

    Aggiornato orologio con file corretto

--- src/components/Orologio/Orologio.jsx
+++ src/components/Orologio/Orologio.jsx
@@ -1,29 +1,30 @@
-import {useState, useEffect} from "react";
-import styles from "./Orologio.module.css";
+import styles from './Orologio.module.css';
+import {useState, useEffect} from 'react';
 
 //funzione figlia
 function OrologioFiglio() {
+  const [ora, setOra] = useState(new Date());
 
-    const [ora, setOra] = useState(new Date());
+  useEffect(() => {
+    // intervallo = identificativo restituito da setInterval
+    const intervallo = setInterval(() => {
+      setOra(new Date());
+    }, 1000);
 
-    useEffect( () => {
-        //intervallo = identificativo restituito da setInterval
-        const intervallo = setInterval(() => {
-            setOra(new Date());
-        }, 1000);
+    return () => clearInterval(intervallo);
+  }, []);
 
-    return () =>clearInterval(intervallo)},[]);
-    
-    return <div className={styles.orologio}>{ora.toLocaleTimeString()}</div>;
-}
+  return <div className={styles.orologio}>{ora.toLocaleTimeString()}</div>;
+} //fine funzione figlia
 
 //Componente padre che monta e smonta la funzione orologio
-export default function Orologio() {
-    const [visibile, setVisibile] = useState(true);
 
-    return (
-        <div>
-            <OrologioFiglio/>
-        </div>
-    );
+export default function Orologio() {
+  const [visible, setVisibile] = useState(true);
+
+  return (
+    <div>
+      <OrologioFiglio />
+    </div>
+  );
 }
