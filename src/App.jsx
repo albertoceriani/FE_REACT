7e7764ce60883eaa6d0afdd9828da07a1b99cf29
albertoceriani <alcerianivr@gmail.com>
2026-06-15 18:02:59 +0200

    Componente RegistraStudente

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // import { useState } from "react";
 import "./App.css";
+import RegistraStudente from "./components/RegistraStudente/RegistraStudente";
 // import ChangeTitle from "./components/ChangeTitle/ChangeTitle";
 // import Orologio from "./components/Orologio/Orologio";
 // import ResettaFocus from "./components/ResettaFocus/ResettaFocus";
-import ContatoreUseRef from "./components/ContatoreUseRef/ContatoreUseRef";
+// import ContatoreUseRef from "./components/ContatoreUseRef/ContatoreUseRef";
 
 function App() {
   return (
     <>
       {/* <Orologio/>
       <ChangeTitle /> 
-      <ResettaFocus />*/}
-      <ContatoreUseRef/>
+      <ResettaFocus />
+      <ContatoreUseRef/>*/}
+      <RegistraStudente />
     </>
   );
 }
